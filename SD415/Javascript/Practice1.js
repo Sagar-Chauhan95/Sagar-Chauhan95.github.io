7c042bce8c47c42dbc0249213793c7aaf75f5c38
Sagar Chauhan <schauhan@miu.edu>
2023-10-31 12:26:34 -0500

    commit

---
+++ SD415/Javascript/Practice1.js
@@ -0,0 +1,31 @@
+import PromptSync from "prompt-sync";
+const prompt =PromptSync()
+
+// let x =String(7);  // way to make string
+// let x1 = ""+7;    // way to make string
+// let y=Number(x);  // way to make number
+// let z = +x;    // way to make number
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof(z));
+// console.log(typeof(x1));
+
+// console.log(Math.sqrt(36));
+
+
+// let x= false;
+// let y= 0;
+// let z = null;
+// console.log(x||y||z);  // null
+// console.log(x??y??z);  // false
+// console.log(x||y);  // 0
+// console.log(x??y);  // false
+
+?
+
+
+ let i = 3;
+
+while (i) {
+  alert( i-- );
+}
